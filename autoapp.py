b603197a10af81ccd7e2cd7a6ccab7c41c533d71
Tom <tommy@daftscience.com>
2016-10-26 08:44:41 -0500

    Update autoapp.py

--- autoapp.py
+++ autoapp.py
@@ -2,8 +2,8 @@
 """Create an application instance."""
 from flask.helpers import get_debug_flag
 
-from pifile.app import create_app
-from pifile.settings import DevConfig, ProdConfig
+from labrador.app import create_app
+from labrador.settings import DevConfig, ProdConfig
 
 CONFIG = DevConfig if get_debug_flag() else ProdConfig
 
