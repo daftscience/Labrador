e1f9d6fd0604812aae1c6189d655e9544189e94f
Tom <tommy@daftscience.com>
2016-10-28 18:22:10 -0500

    update

--- scripts/gpio_functions.py
+++ scripts/gpio_functions.py
@@ -1,6 +1,7 @@
 from gpiozero import Button
 import git
 import subprocess
+from keyboard_simulator import keyboard
 
 GIT_PATH = '/home/pi/projects/labrador/'
 restart_supervisor = "supervisorctl reload"
@@ -10,13 +11,19 @@ def update():
     print("Update")
     g = git.cmd.Git(GIT_PATH)
     g.pull()
-    process = subprocess.Popen(restart_supervisor.split(), stdout=subprocess.PIPE)
+    process = subprocess.Popen(
+        restart_supervisor.split(),
+        stdout=subprocess.PIPE)
     output, error = process.communicate()
     print(output)
 
 update_btn = Button(17)
 update_btn.hold_time = 2
 update_btn.when_held = update
+
+
+other_button = Button(22)
+other_button.when_released = keyboard.f_five
 
 while True:
     pass
