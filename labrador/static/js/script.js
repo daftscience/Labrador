2d55eca30b4f5991d011d233ea7c6480b9134975
Perry <TjPerry@seton.org>
2016-10-28 15:30:06 -0500

    updated css

--- labrador/static/js/script.js
+++ labrador/static/js/script.js
@@ -1,8 +1,11 @@
 (function($, window) {
       $("body").swipe( {
         swipe:function(event, direction, distance, duration, fingerCount, fingerData) {
-          $("#test").text("You swiped " + direction );  
+          $("#test").text("You swiped " + direction );
         },
          threshold:0
       });
+
+      Materialize.updateTextFields();
+
 }).call(this, jQuery, window);
