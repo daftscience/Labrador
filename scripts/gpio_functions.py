162a49a11058831ad9617f1ac78152c23965e110
Perry <TjPerry@seton.org>
2016-10-28 16:40:55 -0500

    update script

--- scripts/gpio_functions.py
+++ scripts/gpio_functions.py
@@ -1,12 +1,18 @@
 from gpiozero import Button
 import git
+import subprocess
 
 GIT_PATH = '/home/pi/projects/labrador/'
+restart_supervisor = "supervisorctl reload"
+
 
 def update():
     print("Update")
     g = git.cmd.Git(GIT_PATH)
     g.pull()
+    process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
+    output, error = process.communicate()
+    print(output)
 
 update_btn = Button(17)
 update_btn.hold_time = 7
