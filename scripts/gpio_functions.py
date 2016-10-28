693747aadf0e473111c2e6b473a5ac346fade3f9
Perry <TjPerry@seton.org>
2016-10-28 16:01:17 -0500

    update script

--- scripts/gpio_functions.py
+++ scripts/gpio_functions.py
@@ -1,8 +1,12 @@
 from gpiozero import Button
+import git
+
 
 
 def update():
     print("Update")
+    g = git.cmd.Git('/home/pi/projects/labrador/')
+    g.pull()
 
 update_btn = Button(17)
 update_btn.hold_time = 7
