90e864b1b8bd4fe5f7cb21722a444c2882eb556c
Tom <tommy@daftscience.com>
2016-10-28 20:45:12 -0500

    update

--- scripts/gpio_functions.py
+++ scripts/gpio_functions.py
@@ -1,36 +1,31 @@
 from gpiozero import Button
 import git
-import subprocess
+from time import sleep
+from subprocess import Popen, PIPE
 from keyboard_simulator import keyboard
 
 GIT_PATH = '/home/pi/projects/labrador/'
 restart_supervisor = "supervisorctl reload"
 
 
-def update():
-    print("Update")
-    g = git.cmd.Git(GIT_PATH)
-    g.pull()
-    process = subprocess.Popen(
-        restart_supervisor.split(),
-        stdout=subprocess.PIPE)
-    output, error = process.communicate()
+class GPIO_HANDLER:
+    GIT = git.cmd.Git(GIT_PATH)
 
-    print(output)
+    def update(self):
+        self.GIT.pull()
+        _process = Popen(
+            ['supervisorctl', 'reload'],
+            stdout=PIPE)
+        output, error = _process.communicate()
+        sleep(2)
+        keyboard.refresh()
 
-update_btn = Button(17)
-update_btn.when_released = update
+if __name__ == '__main__':
+    update_btn = Button(17)
+    update_btn.when_released = GPIO_HANDLER.update
 
-
-refresh_button = Button(22)
-refresh_button.when_released = keyboard.f_five
-
-
-test_button = Button(27)
-test_button.when_released = keyboard.test
-
-while True:
-    pass
+    while True:
+        pass
 
 
 # channel_list = [17, 22, 23, 27]
--- scripts/keyboard_simulator.py
+++ scripts/keyboard_simulator.py
@@ -2,16 +2,10 @@ import uinput
 
 
 class keyboard:
-    DEVICE = uinput.Device([
-        uinput.KEY_E,
-        uinput.KEY_H,
-        uinput.KEY_L,
-        uinput.KEY_O,
-        uinput.KEY_F5
-    ])
+    DEVICE = uinput.Device([uinput.KEY_F5])
 
     @classmethod
-    def f_five(cls, key):
+    def refresh(cls, key):
         print("f5")
         cls.DEVICE.emit_click(uinput.KEY_F5)
 
