4518d56550955b4cfb59d4d8a9d8ea277ed91b7d
Tom <tommy@daftscience.com>
2016-10-28 21:45:12 -0500

    update

--- labrador/assets.py
+++ labrador/assets.py
@@ -3,16 +3,15 @@
 from flask_assets import Bundle, Environment
 import os
 
-print()
 root = os.path.abspath(os.path.dirname(__file__))
 css = Bundle(
-    os.path.join(root, 'static', 'scss/theme.scss',
+    os.path.join(root, 'static', 'scss/theme.scss'),
     filters='scss',
     output='css/common.css',
     depends='scss/*.scss'
 )
 
-js=Bundle(
+js = Bundle(
     'libs/jQuery/dist/jquery.js',
     'libs/materialize/bin/materialize.js',
     'libs/jquery-touchswipe/jquery.touchSwipe.js',
@@ -21,7 +20,7 @@ js=Bundle(
     output='js/common.js'
 )
 
-assets=Environment()
+assets = Environment()
 
 assets.register('js_all', js)
 assets.register('css_all', css)
