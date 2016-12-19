52325ca145165287f812d92aea7d9b3b7fc9c424
Tom <tommy@daftscience.com>
2016-12-18 19:15:36 -0600

    updated

--- autoapp.py
+++ autoapp.py
@@ -1,10 +1,9 @@
 # -*- coding: utf-8 -*-
 """Create an application instance."""
 from flask.helpers import get_debug_flag
-
 from labrador.app import create_app
 from labrador.settings import DevConfig, ProdConfig
 
 CONFIG = DevConfig if get_debug_flag() else ProdConfig
-
 app = create_app(CONFIG)
+app.run()
